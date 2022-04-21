904030e81a75b98916f3c90690d7271d679b0140
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-04-21 13:37:29 +0700

    Convert tree search arguments

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,9 +1,15 @@
+import { sep } from "path";
 import { Settings } from "../interfaces";
 import dataSetting from "./dataSetting";
+import treeSearchSetting from "./treeSearchSetting";
 
 export function prepareCommand (setting: Settings, outputPath?: string) {
+    if (outputPath && outputPath.endsWith(sep))
+        outputPath += sep;
+
     return [
-        ...dataSetting(setting)
+        ...dataSetting(setting),
+        ...treeSearchSetting(setting)
     ]
         .concat('--redo')
         .concat(outputPath ? ['--prefix', outputPath] : [])
---
+++ src/command/treeSearchSetting.ts
@@ -0,0 +1,28 @@
+import { Settings } from "../interfaces";
+import { DefaultPertubationStrength, DefaultUnsuccessfulIterationStop } from "../interfaces/treeSearchSettings";
+export default ({ treeSearch } : Settings) => {
+    let { enabled, unsuccessfulIterationsStop, pertubationStrength, constrainedTreeFile, referenceTreeFile, outgroupTaxa } = treeSearch;
+    let output : string[] = [];
+    if (enabled) output.push('-n', '0');
+    if (unsuccessfulIterationsStop !== DefaultUnsuccessfulIterationStop && unsuccessfulIterationsStop !== null) {
+        output.push('--nstop', unsuccessfulIterationsStop.toString())
+    }
+
+    if (pertubationStrength !== DefaultPertubationStrength && pertubationStrength !== null) {
+        output.push('--perturb', pertubationStrength.toString());
+    }
+
+    if (constrainedTreeFile !== null) {
+        output.push('-g', constrainedTreeFile)
+    }
+
+    if (referenceTreeFile !== null) {
+        output.push('-te', referenceTreeFile);
+    }
+
+    if (outgroupTaxa !== null) {
+        output.push('-o', outgroupTaxa)
+    }
+
+    return output;
+}
