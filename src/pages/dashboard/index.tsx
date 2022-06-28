9c21aafae5c830371b8db91928ebc2865f49189e
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-06-28 22:52:30 +0700

    Show notice when no projects recently opened

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -29,6 +29,83 @@ function Dashboard() {
             <>Loading...</>
         )
 
+    let renderedRecords = (
+        <>
+            {records.map(r => {
+                let valid = true;
+                try {
+                    readSettingsFileSync(r.path);
+                }
+                catch { valid = false };
+
+                let onClick = () => {
+                    navigate({
+                        pathname: "/project",
+                        search: createSearchParams({ path: r.path }).toString()
+                    });
+                    navigate(0);
+                    setSearchParams({
+                        path: r.path
+                    });
+                };
+
+                return (
+                    <tr
+                        key={r.path}
+                        className={`border-y ${valid ? 'hover:bg-slate-200' : ''}`}>
+                        <td className={valid ? 'cursor-pointer' : ''} onClick={valid ? onClick : undefined}>
+                            <span className={valid ? '' : 'text-slate-300 line-through'}>
+                                {r.path}
+                            </span>
+                            {!valid && (
+                                <>
+                                    <br />
+                                    <span className='font-bold text-red-400'>
+                                        Could not read from the setting file (project setting corrupt?)
+                                    </span>
+                                </>
+                            )}
+                        </td>
+                        <td className={valid ? 'cursor-pointer' : ''} onClick={valid ? onClick : undefined}>
+                            <TimeAgo date={r.timestamp} />
+                            <br />
+                            <span className="font-light text-slate-400">
+                                {new Date(r.timestamp).toLocaleString('vi-VN')}
+                            </span>
+                        </td>
+                        <td>
+                            <button
+                                className='p-3 bg-red-300 rounded-lg text-white hover:bg-red-500'
+                                onClick={() => {
+                                    ipcRenderer.callMain('db_remove', r.path)
+                                        .then(load);
+                                }}>
+                                <div className='flex flex-row items-center'>
+                                    <FontAwesomeIcon icon={faClose} />
+                                </div>
+                            </button>
+                        </td>
+                    </tr>
+                )
+            })}
+        </>
+    )
+
+    if (records.length) {
+        renderedRecords = (
+            <tr>
+                <td colSpan={3}>
+                    <div className='text-center bg-gray-300'>
+                        <br />
+                        No projects recently opened.
+                        <br />
+                        &nbsp;
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+
     return (
         <div className='p-2'>
             <h4 className='text-2xl font-bold'>
@@ -45,63 +122,7 @@ function Dashboard() {
                     </tr>
                 </thead>
                 <tbody>
-                    {records.map(r => {
-                        let valid = true;
-                        try {
-                            readSettingsFileSync(r.path);
-                        }
-                        catch { valid = false };
-
-                        let onClick = () => {
-                            navigate({
-                                pathname: "/project",
-                                search: createSearchParams({ path: r.path }).toString()
-                            });
-                            navigate(0);
-                            setSearchParams({
-                                path: r.path
-                            });
-                        };
-
-                        return (
-                            <tr
-                                key={r.path}
-                                className={`border-y ${valid ? 'hover:bg-slate-200' : ''}`}>
-                                <td className={valid ? 'cursor-pointer' : ''} onClick={valid ? onClick : undefined}>
-                                    <span className={valid ? '' : 'text-slate-300 line-through'}>
-                                        {r.path}
-                                    </span>
-                                    {!valid && (
-                                        <>
-                                            <br />
-                                            <span className='font-bold text-red-400'>
-                                                Could not read from the setting file (project setting corrupt?)
-                                            </span>
-                                        </>
-                                    )}
-                                </td>
-                                <td className={valid ? 'cursor-pointer' : ''} onClick={valid ? onClick : undefined}>
-                                    <TimeAgo date={r.timestamp} />
-                                    <br />
-                                    <span className="font-light text-slate-400">
-                                        {new Date(r.timestamp).toLocaleString('vi-VN')}
-                                    </span>
-                                </td>
-                                <td>
-                                    <button
-                                        className='p-3 bg-red-300 rounded-lg text-white hover:bg-red-500'
-                                        onClick={() => {
-                                            ipcRenderer.callMain('db_remove', r.path)
-                                                .then(load);
-                                        }}>
-                                        <div className='flex flex-row items-center'>
-                                            <FontAwesomeIcon icon={faClose} />
-                                        </div>
-                                    </button>
-                                </td>
-                            </tr>
-                        )
-                    })}
+                    {renderedRecords}
                 </tbody>
             </table>
         </div>
