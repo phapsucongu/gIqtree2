c4d097ea44f05556ff726cb788cef098170345aa
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-08-15 13:27:23 +0700

    Implement recent project logging

--- src/pages/right/index.tsx
+++ src/pages/right/index.tsx
@@ -1,3 +1,4 @@
+import { ipcRenderer } from "electron-better-ipc"
 import { normalize } from "path"
 import { Routes, Route } from "react-router-dom"
 import { AppRoute } from "../../routes"
@@ -8,7 +9,9 @@ export default () => {
     return (
         <Routes>
             <Route path={AppRoute.Dashboard} element={<Dashboard />} />
-            <Route path={normalize(AppRoute.Project + '/:path')}  element={<Project />} />
+            <Route
+                path={normalize(AppRoute.Project + '/:path')}
+                element={<Project onOpenProject={path => ipcRenderer.callMain('db_record', path)} />} />
         </Routes>
     )
 }
--- src/pages/right/project.tsx
+++ src/pages/right/project.tsx
@@ -1,10 +1,13 @@
 import { normalize } from "path";
+import { useEffect } from "react";
 import { useMatch } from "react-router-dom"
 import { AppRoute } from "../../routes";
 
-export default () => {
+export default ({ onOpenProject } : { onOpenProject?: (path: string) => void }) => {
     let m = useMatch(normalize(AppRoute.Project + '/:path'))!;
     let { path } = m.params;
+
+    useEffect(() => onOpenProject?.(path!), [onOpenProject, path]);
     return (
         <div className='pl-6'>
             {path}
