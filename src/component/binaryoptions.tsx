3e4ed9add381d100bccf20de11871362d704bba3
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-09-13 21:19:45 +0700

    Add model settings

---
+++ src/component/binaryoptions.tsx
@@ -0,0 +1,25 @@
+function BinaryOptions(
+    { value, truthyText, falsyText, onChange }
+    : { value: boolean, truthyText: string, falsyText: string, onChange?: (v: boolean) => void }
+) {
+    return (
+        <div className="w-full input-bordered flex flex-row items-center gap-2">
+            <select
+                className="p-2 w-full bg-transparent no-select-arrow"
+                value={+value}
+                onChange={e => onChange?.(!!+e.target.value)}>
+                <option value={1}>
+                    {truthyText}
+                </option>
+                <option value={0}>
+                    {falsyText}
+                </option>
+            </select>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default BinaryOptions;
--- src/pages/right/project/settings/model.tsx
+++ src/pages/right/project/settings/model.tsx
@@ -1,12 +1,145 @@
+import BinaryOptions from "../../../../component/binaryoptions";
 import { SequenceType } from "../../../../interfaces/dataSettings";
-import { ModelSettings } from "../../../../interfaces/modelSettings";
+import { AutoMergePartitions, AutoMergePartitionsAlgorithms, getAvailableFrequencies, ModelSettings, RHASModel, RHASModels, StateFrequency, SubstitutionModel, SubstitutionModels } from "../../../../interfaces/modelSettings";
 import { SettingCategoryCommonProp } from "./settingCategoryCommonProps";
 
-function Model({ settings, onChange, sequenceType, isMultipleGene } : SettingCategoryCommonProp<ModelSettings> & {
+function Model({ settings, onChange, sequenceType, isMultipleGene }: SettingCategoryCommonProp<ModelSettings> & {
     sequenceType: SequenceType | undefined,
     isMultipleGene: boolean
 }) {
-    return (<>model</>)
+    let { proportionOfInvariableSites, ascertainmentBiasCorrection } = settings ?? {};
+    let availableModels: (SubstitutionModel | undefined)[] = SubstitutionModels
+        .filter(e => e.sequenceType === sequenceType || sequenceType === undefined)
+        .map(e => e.models)
+        .flat();
+    availableModels.unshift(undefined);
+
+    let availableFrequencies = getAvailableFrequencies(sequenceType);
+
+    return (
+        <div className="flex flex-col gap-6">
+            {isMultipleGene && (
+                <div>
+                    <b className="pb-2">
+                        Auto merge partitions
+                    </b>
+                    <br />
+                    <select
+                        className="p-2 w-full input-bordered bg-transparent"
+                        onChange={e => onChange?.({
+                            ...settings,
+                            autoMergePartitions: (e.target.value || undefined) as AutoMergePartitions | undefined
+                        })}
+                        value={settings?.autoMergePartitions}>
+                        {
+                            [{ name: 'None', type: undefined }, ...AutoMergePartitionsAlgorithms]
+                                .map(option => {
+                                    return (
+                                        <option value={option.type}>
+                                            {option.name}
+                                        </option>
+                                    )
+                                })
+                        }
+                    </select>
+                </div>
+            )}
+            <div>
+                <b className="pb-2">
+                    Subtitution model
+                </b>
+                <br />
+                <select
+                    className="p-2 w-full input-bordered bg-transparent"
+                    onChange={e => onChange?.({
+                        ...settings,
+                        substitutionModel: (e.target.value || undefined) as SubstitutionModel | undefined
+                    })}
+                    value={settings?.substitutionModel}>
+                    {
+                        availableModels
+                            .map(option => {
+                                return (
+                                    <option value={option}>
+                                        {option ?? 'Automatic'}
+                                    </option>
+                                )
+                            })
+                    }
+                </select>
+            </div>
+            <div>
+                <b className="pb-2">
+                    Allow proportion of invariable sites
+                </b>
+                <br />
+                <BinaryOptions
+                    value={proportionOfInvariableSites ?? false}
+                    truthyText="On"
+                    falsyText="Off"
+                    onChange={v => onChange?.({ ...settings, proportionOfInvariableSites: v })} />
+            </div>
+            <div>
+                <b className="pb-2">
+                    RHAS model
+                </b>
+                <br />
+                <select
+                    className="p-2 w-full input-bordered bg-transparent"
+                    onChange={e => onChange?.({
+                        ...settings,
+                        rhasModel: (e.target.value || undefined) as RHASModel | undefined
+                    })}
+                    value={settings?.substitutionModel}>
+                    {
+                        [{ name: 'None', type: undefined }, ...RHASModels]
+                            .map(option => {
+                                return (
+                                    <option value={option.type}>
+                                        {option.name}
+                                    </option>
+                                )
+                            })
+                    }
+                </select>
+            </div>
+            <div>
+                <b className="pb-2">
+                    State frequency
+                </b>
+                <br />
+                <select
+                    className="p-2 w-full input-bordered bg-transparent"
+                    onChange={e => onChange?.({
+                        ...settings,
+                        stateFrequency: (e.target.value || undefined) as StateFrequency | undefined
+                    })}
+                    value={settings?.substitutionModel}>
+                    {
+                        [{ name: 'None', type: undefined }, ...availableFrequencies]
+                            .map(option => {
+                                return (
+                                    <option value={option.type}>
+                                        {option.name}
+                                    </option>
+                                )
+                            })
+                    }
+                </select>
+            </div>
+            <div>
+                <b className="pb-2">
+                    Ascertainment bias correction
+                </b>
+                <br />
+                <BinaryOptions
+                    value={ascertainmentBiasCorrection ?? false}
+                    truthyText="On"
+                    falsyText="Off"
+                    onChange={v => onChange?.({ ...settings, ascertainmentBiasCorrection: v })} />
+            </div>
+        </div>
+    )
 }
 
 export default Model;
