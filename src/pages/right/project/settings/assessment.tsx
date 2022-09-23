798c2292804751139fcc4207a371f957628e78d1
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-09-23 23:53:27 +0700

    Add assessment settings

--- src/pages/right/project/settings/assessment.tsx
+++ src/pages/right/project/settings/assessment.tsx
@@ -1,9 +1,12 @@
-import { AssessmentSettings, BootstrapMethod, BootstrapMethods } from "../../../../interfaces/assessmentSettings";
+import BinaryOptions from "../../../../component/binaryoptions";
+import SettingRowFile from "../../../../component/settingrowfile";
+import { AssessmentSettings, BootstrapMethod, BootstrapMethods, DefaultBootstrapMethodReplicate } from "../../../../interfaces/assessmentSettings";
 import { SettingCategoryCommonProp } from "./settingCategoryCommonProps";
 
 function Assessment({ settings, onChange } : SettingCategoryCommonProp<AssessmentSettings> & {
     isMultipleGene: boolean
 }) {
+    let { gcf, scf } = settings ?? {};
     return (
         <div className="flex flex-col gap-6">
             <div>
@@ -34,14 +37,49 @@ function Assessment({ settings, onChange } : SettingCategoryCommonProp<Assessmen
                 <b className="pb-2">
                     Number of bootstrapping replicates
                 </b>
-                {/* <input
+                <input
+                    className="py-1 px-2 w-full input-bordered bg-transparent"
+                    type="number"
+                    onChange={e => onChange?.({
+                        ...settings,
+                        bootstrapMethodReplicate: (e.target.valueAsNumber || undefined)
+                    })}
+                    value={settings?.bootstrapMethodReplicate ?? DefaultBootstrapMethodReplicate} />
+            </div>
+            <div>
+                <b className="pb-2">
+                    Enable gCF
+                </b>
+                <br />
+                <BinaryOptions
+                    value={gcf?.enabled ?? false}
+                    truthyText="On"
+                    falsyText="Off"
+                    onChange={v => onChange?.({ ...settings, gcf: { ...gcf, enabled: v } })} />
+            </div>
+            <div>
+                <b className="pb-2">
+                    sCF quartet number (0 to disable sCF)
+                </b>
+                <input
+                    min={0}
                     className="px-1 py-2 w-full input-bordered bg-transparent"
                     type="number"
                     onChange={e => onChange?.({
                         ...settings,
-                        // re: (e.target.valueAsNumber || undefined)
+                        scf: { ...scf, quartet: e.target.valueAsNumber || undefined }
                     })}
-                    value={settings?.thread} /> */}
+                    value={scf?.quartet ?? 0} />
+            </div>
+            <div>
+                <b className="pb-2">
+                    Gene tree (leave empty to generate)
+                </b>
+                <SettingRowFile
+                    isFile
+                    name="Gene tree (leave empty to generate)"
+                    file={gcf?.geneTree}
+                    onChange={file => onChange?.({ ...settings, gcf: { ...gcf, geneTree: file || undefined } })} />
             </div>
         </div>
     )
