89a7d2f2acbc6cdf1631e1e63494400c32769192
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2022-10-05 21:33:51 +0700

    Check for execution state more frequently

--- src/pages/right/project/hooks/useExecutionState.ts
+++ src/pages/right/project/hooks/useExecutionState.ts
@@ -13,7 +13,7 @@ function useExecutionState(path: string) {
     }
 
     useEffect(() => {
-        let interval = setInterval(refresh, 500);
+        let interval = setInterval(refresh, 100);
         return () => clearInterval(interval);
     })
 
