35e75ba3d532dc537c50fb054a7474a8c1c9aaa3
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2023-12-16 00:00:57 +0700

    Remove unused files

--- src/state/hook.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch as dispatch, useSelector as selector } from 'react-redux'
-import type { RootState, AppDispatch } from './store'
-
-export const useDispatch: () => AppDispatch = dispatch
-export const useSelector: TypedUseSelectorHook<RootState> = selector
