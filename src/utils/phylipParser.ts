1fd0dcebbd94c5dcefc8e10075945f692153c3e4
Vo Minh Duc <semper.vigilantem.618@gmail.com>
2024-05-14 20:04:03 +0700

    Remove max column in matrix view

--- src/utils/phylipParser.ts
+++ src/utils/phylipParser.ts
@@ -1,5 +1,4 @@
 export interface PhylipData {
-    max: string[];
     taxa: {
         name: string,
         taxon: string[]
@@ -9,7 +8,7 @@ export interface PhylipData {
 export function phylipParse(content: string): PhylipData {
     let lines = content.split('\n');
 
-    let [taxaCount, taxaLength] = lines[0].trim().split(' ').map(a => +(a.trim()));
+    let [taxaCount] = lines[0].trim().split(' ').map(a => +(a.trim()));
     let taxa = lines.slice(1).slice(0, taxaCount)
         .map(line => {
             let pieces = line.trim().split(' ');
@@ -21,25 +20,7 @@ export function phylipParse(content: string): PhylipData {
             }
         });
 
-    let max = new Array<string>(taxaLength).fill('');
-
-    for (let column = 0; column < taxaLength; column++) {
-        let m = new Map<string, number>();
-        for (let { taxon } of taxa) {
-            let at = taxon[column].toLowerCase();
-            if (at) {
-                let v = m.get(at) ?? 0;
-                m.set(at, v + 1);
-            }
-        }
-
-        let mostOccurence = [...m.entries()].sort((a, b) => a[1] - b[1]).pop()![0];
-
-        max[column] = mostOccurence;
-    }
-
     return {
-        max,
         taxa
     }
 
